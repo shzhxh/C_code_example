7a3a4ca26bc8eeee366c73ec7e999a874631ea5e
shzhxh <shzhxh@gmail.com>
2024-06-20 16:03:55 +0800

    添加例子:静态库

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # C_code_example
 C代码示例
+
+## 静态库
+
+```sh
+cd static_lib
+make    # 生成静态库libm.a
+./main.out  # 使用静态库
+```
---
+++ static_lib/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "m.h"
+
+int main(void){
+    printf("add: %d, sub: %d\n", add(1,2), sub(1,2));
+}
